e85b552faac34fe80ff98d0d509329b47ea44544
Leonel Rivero <103612900+leorivero91@users.noreply.github.com>
2022-05-01 19:05:07 -0300

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+let dato1 = document.getElementById("dato1");
+let dato2 = document.getElementById("dato2");
+
+let base: number = Number(dato1.value);
+let altura: number = Number(dato2.value);
+let area: number = base*altura;
+
+
+btnEnviarAltura.addEventListener("click" , () => {
+  // TU CODIGO VA AQUI, POR EJEMPLO:
+  console.log("El area es ",(dato1.value*dato2.value);
+  //}
+  });
